3534fe68a2648ce9d5a0f80f3a66e703f2b5e679
Brad <brad@Brads-MacBook-Air.local>
2015-02-16 00:39:32 +1100

    debugging

--- lib/net/tcp_client/tcp_client.rb
+++ lib/net/tcp_client/tcp_client.rb
@@ -244,7 +244,7 @@ module Net
       @close_on_error         = params.delete(:close_on_error)
       @close_on_error         = true if @close_on_error.nil?
       @logger                 = params.delete(:logger)
-
+      $stdout.puts "@use_ssl = #{@use_ssl}"
       unless @servers = params.delete(:servers)
         raise "Missing mandatory :server or :servers" unless server = params.delete(:server)
         @servers = [ server ]
